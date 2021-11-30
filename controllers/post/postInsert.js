ef780b2c257cf66eb8ac9f3564d1f9f696674cd4
mewan19 <77052354+mewan19@users.noreply.github.com>
2021-11-30 14:31:40 +0500

    Added SEO fields

--- controllers/post/postInsert.js
+++ controllers/post/postInsert.js
@@ -1,8 +1,12 @@
+const { response } = require("express");
+
 const filter = {}
 const  postInsert=(connection)=>(req,res)=>{ 
     getValueForNextSequence(connection,"item_id")
-    const {title,slug,link,description,excerpt,featuredImage,featured,date,category,author,tags}=req.body
+    const {title,slug,link,description,excerpt,featuredImage,featured,date,category,author,tags, seo}=req.body
     const newDate = new Date();
+    
+    
 
     title? filter['title']=title : 0
     slug? filter['slug']=slug : filter['slug']=title.toLowerCase().replace(' ','-')
@@ -15,6 +19,12 @@ const  postInsert=(connection)=>(req,res)=>{
     category? filter['category']=category : 0
     author? filter['author']=author : 0
     tags? filter['tags']=tags : filter['tags']=tags
+    
+     
+    seo?filter['seo']=seo:0
+    
+    
+    res.send(req.body);
 
     connection.then(client => {
     const post = client.db('news').collection('post')  
