c326193f768f5a784ba93a0a7a6f32549d7fa5e4
mewan19 <77052354+mewan19@users.noreply.github.com>
2022-01-24 11:47:03 +0500

    Updated tags API

--- controllers/tagsGet.js
+++ controllers/tagsGet.js
@@ -16,33 +16,31 @@ const tagsGet=(connection)=>(req,res)=>{
     const quotesCollection = post.aggregate().toArray()
     .then(results => {
 
-      // results.map(result =>{
-      //     result['thumbnail']={
-      //       '__typename': 'ImageSharp',
-      //       'ImageSharp_vertical': {
-      //         'layout': 'constrained',
-      //         'backgroundColor': '#787898',
-      //         'images': {
-      //           'fallback': {
-      //             'src': result.featuredImage,
-      //             'srcSet': '',
-      //             'sizes': '(min-width: 380px) 380px, 100vw'
-      //           },
-      //           'sources': [
-      //             {
-      //               'srcSet': result.featuredImage,
-      //               'type': 'image/jpg',
-      //               'sizes': '(min-width: 380px) 380px, 100vw'
-      //             }
-      //           ]
-      //         },
-      //         'width': 380,
-      //         'height': 290
-      //       }
-      //     }
-      // })
-
+  
       results.map(result =>{ 
+        result['thumbnail']={
+                '__typename': 'ImageSharp',
+                'ImageSharp_vertical': {
+                  'layout': 'constrained',
+                  'backgroundColor': '#787898',
+                  'images': {
+                    'fallback': {
+                      'src': result.featuredImage,
+                      'srcSet': '',
+                      'sizes': '(min-width: 380px) 380px, 100vw'
+                    },
+                    'sources': [
+                      {
+                        'srcSet': result.featuredImage,
+                        'type': 'image/jpg',
+                        'sizes': '(min-width: 380px) 380px, 100vw'
+                      }
+                    ]
+                  },
+                  'width': 380,
+                  'height': 290
+                }
+              }
         result.tags.split(',').map(result1 =>{
           tags.includes(result1.toLowerCase())? (0) :
         (
@@ -56,12 +54,14 @@ const tagsGet=(connection)=>(req,res)=>{
           tags.push(result1.toLowerCase()),
           posts[result1.toLowerCase()]= []
         )
-        posts[result1.toLowerCase()].push(result)
+       posts[result1.toLowerCase()].push(result)
         })
       })
 
       respons.map(result =>{ 
+        
         result.posts=posts[result.tag]
+        
       })
     
       try {
